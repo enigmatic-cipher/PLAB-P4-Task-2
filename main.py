1e6da37d691ab7cc2e5ac1daad0a9e1e4bfcc3f0
Amit <87896992+enigmatic-cipher@users.noreply.github.com>
2022-01-16 14:14:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+Given n as input. For every number from 1 to n:
+ print 'X' if the number is even
+ print 'Y' otherwise
+"""
+
+n = int(input("Enter Number: "))
+if (n%2==0):
+  print("X")
+else:
+  print("Y")
